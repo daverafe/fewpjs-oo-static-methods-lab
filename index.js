4dc044fa7b5567d475e0bf087fec6d20d024cb77
David Rafe <david.rafe@gmail.com>
2021-06-14 17:30:05 -0400

    commit tree f6a63d6cb34f4d4154ecc5e4d8655f93cad764fd

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-'/\s/g]+/g, '')
+  }
+
+  static titleize(string) {
+    const littleWords = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
+    let array = string.split(' ')
+  let newArray = array.map(word => {
+      if(littleWords.includes(word) && word != array[0]) {
+        return word 
+      } else {
+        return this.capitalize(word)
+      }
+    })
+    return newArray.join(' ')
+  }
+
 }
